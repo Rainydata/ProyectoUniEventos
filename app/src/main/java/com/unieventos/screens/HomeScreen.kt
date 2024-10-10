f81359660882478439611b328f9fcf660cf5e03f
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-10-09 23:41:50 -0500

    homescreen iniciado para la lista de eventos

--- app/src/main/java/com/unieventos/screens/HomeScreen.kt
+++ app/src/main/java/com/unieventos/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.unieventos.screens
 
+import ItemEvento
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -8,31 +9,67 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
+
+
 
 @Composable
 fun HomeScreen(
     onNavigationToEditProfile: () -> Unit
-) {
-    val context = LocalContext.current
 
-    Scaffold { padding ->
+){
+    Scaffold {
+            paddingValues ->
         Column(
             modifier = Modifier
-                .padding(padding)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .padding(paddingValues)
+                .verticalScroll(rememberScrollState())
         ) {
-            Spacer(modifier = Modifier.height(20.dp))
+            ItemEvento(
+                "Obra de Teateo 1",
+                "Pereira"
+            )
 
-            Button(
-                onClick = {
-                    onNavigationToEditProfile()
-                }
-            ) {
-                Text(text = "Editar perfil")
-            }
+            ItemEvento(
+                "Concierto Andrés Cepeda",
+                "Bogotá"
+
+            )
+            ItemEvento(
+                "Ópera",
+                "Cali"
+
+            )
+
         }
     }
 }
+
+//@Composable
+//fun HomeScreen(
+//    onNavigationToEditProfile: () -> Unit
+//) {
+//    val context = LocalContext.current
+//
+//    Scaffold { padding ->
+//        Column(
+//            modifier = Modifier
+//                .padding(padding)
+//                .fillMaxSize(),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center
+//        ) {
+//            Spacer(modifier = Modifier.height(20.dp))
+//
+//            Button(
+//                onClick = {
+//                    onNavigationToEditProfile()
+//                }
+//            ) {
+//                Text(text = "Editar perfil")
+//            }
+//        }
+//    }
+//}
